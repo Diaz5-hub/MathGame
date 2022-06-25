1a24723ed96e845aec6e368dc2f44a78faf9a26a
Alexander Diaz <AlexDiaz6022000@gmail.com>
2022-06-25 12:37:47 -0700

    Initial Commit

---
+++ MathGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MathGame
+//
+//  Created by Alexander Diaz on 6/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
